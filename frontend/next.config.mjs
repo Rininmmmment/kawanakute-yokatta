ae63f0ee086850811074e047217b1f222b4b8f63
Rininmmmment <rinr10693otes@gmail.com>
2025-02-10 12:09:27 +0900

    ログインエラー修正

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,21 +1,24 @@
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Cross-Origin-Opener-Policy",
+  //           value: "same-origin-allow-popups",
+  //         },
+  //         {
+  //           key: "Cross-Origin-Embedder-Policy",
+  //           value: "require-corp",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
